6095467dc4017f76eebd15a8e83b8c76d780af15
Hanjinxi <han1919005563@163.com>
2020-06-01 17:08:24 +0800

    service层的service接口方法类

---
+++ src/com/edu/service/OrderService.java
@@ -0,0 +1,35 @@
+package com.edu.service;
+
+import java.util.List;
+import java.util.Map;
+
+import com.edu.entity.Order;
+
+/**
+ * 订单service接口
+ * @author Administrator
+ *
+ */
+public interface OrderService {
+
+	/**
+	 * 查询订单
+	 * @param map
+	 * @return
+	 */
+	public List<Order> findOrder(Map<String,Object> map);
+	
+	/**
+	 * 查询订单记录数
+	 * @param map
+	 * @return
+	 */
+	public Long getTotalOrder(Map<String,Object> map);
+	
+	/**
+	 * 通过Id查找实体
+	 * @param id
+	 * @return
+	 */
+	public Order findById(Integer id);
+}
